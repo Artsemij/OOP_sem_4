1317c85476ec1a2cffda3ff23b77bda77a9bf4b9
Artur <art.igor.a1986@gmail.com>
2023-09-16 12:44:31 +0300

    Create TeacherService class.

---
+++ TeacherService.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+public class TeacherService {
+    private List<Teacher> teachers;
+    public TeacherService(){
+        this.teachers = new ArrayList<>();
+    }
+    public void addTeacher(Teacher teacher){
+        //добавление учителя в список
+        teachers.add(teacher);
+    }
+    public void editTeacher(Teacher teacher){
+        //изменение информации о конкретном учителе
+        teachers.set(index, teacher);
+    }
+    public List<Teacher> getAllTeachers(){
+        //возвращает список всех учителей
+        return teachers;
+    }
+
+}
