2ccd71da4bf52fabd3b28f3d43e0b999ae2efcf4
Artur <art.igor.a1986@gmail.com>
2023-09-16 12:32:00 +0300

    Create TeacherComparator class.

---
+++ TeacherComparator.java
@@ -0,0 +1,6 @@
+import java.util.Comparator;
+public class TeacherComparator implements Comparator<Teacher> {
+    public int compare(Teacher teacher1, Teacher teacher2){
+        return teacher1.getAge() - teacher2.getAge();
+    }
+}
