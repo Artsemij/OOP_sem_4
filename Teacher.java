b36132e4c689d3db592d51a53e8973a1e5f267d8
Artur <art.igor.a1986@gmail.com>
2023-09-16 12:20:57 +0300

    Create Teacher class.

---
+++ Teacher.java
@@ -0,0 +1,15 @@
+public class Teacher{
+    private String name;
+    private int age;
+
+    public Teacher(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+    public String getName(){
+        return name;
+    }
+    public int getAge(){
+        return age;
+    }
+}
